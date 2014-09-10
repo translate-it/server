99e6d73c4fff24a93ab9a2d91be17ee040ea0bce
trothe <tobias.rothe@spoutnik-mobile.com>
2014-09-10 20:07:14 +0200

    unescape single and double quotes when importing Android resources

--- src/com/amadeus/ori/translate/importers/AndroidImporter.java
+++ src/com/amadeus/ori/translate/importers/AndroidImporter.java
@@ -10,12 +10,13 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.amadeus.ori.translate.domain.dto.TranslationImportDTO;
 
-@ImporterName(name="android", description="Android values XML file")
+@ImporterName(name = "android", description = "Android values XML file")
 public class AndroidImporter implements Importer {
 
 	private static final Log LOG = LogFactory.getLog(AndroidImporter.class);
@@ -26,32 +27,32 @@ public class AndroidImporter implements Importer {
 
 		// Output translations list
 		List<TranslationImportDTO> results = null;
-		
+
 		// Parse the incoming XML
 		XMLInputFactory factory = XMLInputFactory.newInstance();
 		XMLStreamReader reader;
-		
+
 		try {
 			reader = factory.createXMLStreamReader(is);
 		} catch (XMLStreamException e) {
 			LOG.warn("Unable to read XML stream", e);
 			throw new IOException(e);
-		}		
+		}
 		try {
-			while(reader.hasNext()){
+			while (reader.hasNext()) {
 				reader.next();
-			    int event = reader.getEventType();
+				int event = reader.getEventType();
 
-	            switch (event) {
-	            case XMLStreamConstants.START_ELEMENT:
-			        String name = reader.getLocalName();
-			        if ("resources".equals(name)) {
-			        	results = parseResources(reader);
-			        } else {
-			        	LOG.warn("Unexpected element: " + name);
-			        }
-	            	break;
-	            }
+				switch (event) {
+				case XMLStreamConstants.START_ELEMENT:
+					String name = reader.getLocalName();
+					if ("resources".equals(name)) {
+						results = parseResources(reader);
+					} else {
+						LOG.warn("Unexpected element: " + name);
+					}
+					break;
+				}
 			}
 		} catch (XMLStreamException e) {
 			LOG.warn("Unable to parse XML", e);
@@ -66,55 +67,61 @@ public class AndroidImporter implements Importer {
 				LOG.warn("Unable to close XML stream", e);
 			}
 		}
-		
+
 		return results;
 	}
 
-	private List<TranslationImportDTO> parseResources(XMLStreamReader reader) throws Exception {
+	private List<TranslationImportDTO> parseResources(XMLStreamReader reader)
+			throws Exception {
 		List<TranslationImportDTO> results = new ArrayList<TranslationImportDTO>();
 
-		while(reader.hasNext()) {
+		while (reader.hasNext()) {
 			reader.next();
-		    int event = reader.getEventType();
+			int event = reader.getEventType();
 
-            switch (event) {
-            case XMLStreamConstants.START_ELEMENT:
-		        String name = reader.getLocalName();
-		        if ("string".equals(name)) {
-		        	TranslationImportDTO translation = parseString(reader);
-		        	if (translation != null)
-		        		results.add(translation);
-		        } else {
-		        	LOG.warn("Unexpected element: " + name);
-		        }
-            	break;
-            case XMLStreamConstants.END_ELEMENT:
-		        String endEltName = reader.getLocalName();
-		        if ("resources".equals(endEltName)) {
-		        	return results;
-		        } else {
-		        	LOG.warn("Unexpected end element: " + endEltName);
-		        }
-		        
-            	break;
-            }
+			switch (event) {
+			case XMLStreamConstants.START_ELEMENT:
+				String name = reader.getLocalName();
+				if ("string".equals(name)) {
+					TranslationImportDTO translation = parseString(reader);
+					if (translation != null)
+						results.add(translation);
+				} else {
+					LOG.warn("Unexpected element: " + name);
+				}
+				break;
+			case XMLStreamConstants.END_ELEMENT:
+				String endEltName = reader.getLocalName();
+				if ("resources".equals(endEltName)) {
+					return results;
+				} else {
+					LOG.warn("Unexpected end element: " + endEltName);
+				}
+
+				break;
+			}
 		}
-		
+
 		return results;
 	}
 
-	private TranslationImportDTO parseString(XMLStreamReader reader) throws Exception {
+	private TranslationImportDTO parseString(XMLStreamReader reader)
+			throws Exception {
 
 		String key = reader.getAttributeValue(null, "name");
 		String translation = reader.getElementText();
-		
-    	// Store the translation
+		if (StringUtils.contains(translation, '\\')) {
+			translation = translation.replace("\\\"", "\"").replace("\\'", "'");
+		}
+
+		// Store the translation
 		TranslationImportDTO item = new TranslationImportDTO(key, translation);
-		
+
 		if (item.hasKey()) {
 			return item;
 		} else {
-			throw new Exception("Invalid format: key=\"" + key + "\", value=\"" + translation + "\"");
+			throw new Exception("Invalid format: key=\"" + key + "\", value=\""
+					+ translation + "\"");
 		}
 
 	}
